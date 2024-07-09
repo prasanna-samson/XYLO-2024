4de490be3445446b71f091a19931c577f96013a8
prasanna <prasanna.xyloinc.2024@gmail.com>
2024-07-09 20:12:36 +0530

    created support page

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,20 @@ import Whyus from './components/whyUspage/Whyus';
 import Sap from './components/sapService/Sap';
 import Slidebar from './components/slide/Slidebar';
 import Saptech from './components/sapTech/Saptech';
+import Sapsupport from './components/maintain/Sapsupport';
 
 function App() {
  
 
   return (
     <>
-   <NavBar/>
-  <Homepage/>
-  <Whyus></Whyus>
-  <Sap/>
- <Slidebar/>
- <Saptech></Saptech>
+    <NavBar/>
+    <Homepage/>
+    <Whyus></Whyus>
+    <Sap/>
+    <Slidebar/>
+    <Saptech></Saptech>
+    <Sapsupport></Sapsupport>
   </>
   )
 }
---
+++ src/components/maintain/Sapsupport.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import image from "../../assets/support.png"
+const Sapsupport:React.FC = () => {
+  return (
+    <div className='p-20 h-screen bg-xylo grid grid-cols-12'>
+
+    <div className='max-h-full col-span-7 flex flex-col justify-center p-10 col-start-1'>
+        <h1 className='text-6xl  font-semibold mb-10'>SAP Support and Maintenance</h1>
+
+        <p className='text-base'>At XYLOINC, we specialize in supporting our clients through both the implementation and ongoing application management services (AMS) phases.
+        Our dedicated team ensures seamless transitions during implementation and provides comprehensive support to optimize performance and maximize the value of our solutions for our clients.</p>
+    </div>
+    <div className='col-span-5'>
+      <img src={image}/>
+    </div>
+
+
+    </div>
+  )
+}
+
+export default Sapsupport
